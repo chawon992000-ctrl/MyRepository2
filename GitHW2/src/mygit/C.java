269ac39e94028732724ae03d579cfb8f0d7a4318
Chawon <Chawon@LAPTOP>
2025-12-09 13:16:36 +0900

    master 다녀감(c 수정)

--- GitHW2/src/mygit/C.java
+++ GitHW2/src/mygit/C.java
@@ -4,6 +4,7 @@ public class C {
 	
 	public static void main(String args[]) {
 		System.out.println("Class C입니다.");
+		System.out.println("Class C에 master 다녀감");
 		System.out.println();
 
 		A a = new A();
